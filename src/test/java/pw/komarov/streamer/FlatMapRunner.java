bdac1bf0292e92aa33e909557117518ab36777f6
Andrey Komarov (k0m@) <andrey@komarov.pw>
2021-06-25 10:43:46 +0300

    FlatMapRunner modification (500 ms duration for each object creation)

--- src/test/java/pw/komarov/streamer/FlatMapRunner.java
+++ src/test/java/pw/komarov/streamer/FlatMapRunner.java
@@ -7,25 +7,27 @@ public class FlatMapRunner {
         final Stream<Person> personsStream =
                 Streamer
                         .generate(() -> {
-                                final Person person = PersonUtils.generateRandomPerson();
-                                if ((person.id & 1) == 0) //odd id?...
-                                    try { Thread.sleep(1000); } catch (Exception ignored) {} //... if id is odd then sleep
-
-                                return person; //generated person
-                            })
-                        .limit(10) //ten persons is enough
-        ;
+                            new Sleep(500);
+                            return PersonUtils.generateRandomPerson(); //generated person
+                        }).limit(10) //ten persons is enough
+                ;
 
         final Stream<String> personDetailsStream =
                 personsStream.flatMap(person -> Streamer.of(
-                    "---------------------",
-                    "ID: " + person.id,
-                    "Name: " + person.name,
-                    "Gender: " + person.gender,
-                    "Age: " + person.age
+                        "---------------------",
+                        "ID: " + person.id,
+                        "Name: " + person.name,
+                        "Gender: " + person.gender,
+                        "Age: " + person.age
                 ))
-        ;
+                ;
 
         personDetailsStream.forEach(System.out::println);
+    }
+
+    private static class Sleep {
+        private Sleep(long millis) {
+            try { Thread.sleep(millis); } catch (InterruptedException ignored) {}
+        }
     }
 }
