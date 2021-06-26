8e8c8b904c6ecc24803e7a7e22f5b2191c8084ca
Andrey Komarov (k0m@) <andrey@komarov.pw>
2021-06-26 16:14:56 +0300

    additional run(test) cases added to FlatMapRunner

--- src/test/java/pw/komarov/streamer/FlatMapRunner.java
+++ src/test/java/pw/komarov/streamer/FlatMapRunner.java
@@ -1,28 +1,49 @@
 package pw.komarov.streamer;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class FlatMapRunner {
     public static void main(String[] args) {
-        final Stream<Person> personsStream =
-                Streamer
-                        .generate(() -> {
-                            new Sleep(500);
-                            return PersonUtils.generateRandomPerson(); //generated person
-                        }).limit(10) //ten persons is enough
-                ;
+        System.out.println("Test case #1 (sequential FlatMap)");
+            Streamer
+                    .generate(() -> {
+                        new Sleep(500);
+                        return PersonUtils.generateRandomPerson(); //generated person
+                    }).limit(10) //ten persons is enough
+            .flatMap(person -> Streamer.of(
+                    "---------------------",
+                    "ID: " + person.id,
+                    "Name: " + person.name,
+                    "Gender: " + person.gender,
+                    "Age: " + person.age
+            )).forEach(System.out::println);
 
-        final Stream<String> personDetailsStream =
-                personsStream.flatMap(person -> Streamer.of(
-                        "---------------------",
-                        "ID: " + person.id,
-                        "Name: " + person.name,
-                        "Gender: " + person.gender,
-                        "Age: " + person.age
-                ))
-                ;
+        final AtomicInteger ai = new AtomicInteger();
 
-        personDetailsStream.forEach(System.out::println);
+        System.out.println("\nTest case #2 (empty odd result)");
+        Streamer
+                .generate(() -> {
+                    new Sleep(500);
+                    return PersonUtils.generateRandomPerson(); //generated person
+                }).limit(10) //ten persons is enough
+                .flatMap(person -> {
+                    if ((ai.incrementAndGet() & 1) == 0)
+                        return Streamer.empty();
+                    else
+                        return Streamer.of(
+                                "---------------------",
+                                "ID: " + person.id,
+                                "Name: " + person.name,
+                                "Gender: " + person.gender,
+                                "Age: " + person.age);
+                }).forEach(System.out::println);
+
+        System.out.println("\nTest case #3 (FlatMap from empty parent stream)");
+        Streamer
+                .empty()
+                .flatMap(o -> Stream.of(ai.get(), ai.get() * -1))
+                .forEach(System.out::println);
     }
 
     private static class Sleep {
